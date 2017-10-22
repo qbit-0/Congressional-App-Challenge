e4946f6ce1cf71765543ad0d8183027d314f11d2
KeyboardCreature <duypham12241999@gmail.com>
2017-10-22 12:03:47 -0700

    Reformat

--- test/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ test/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -4,9 +4,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+public class DesktopLauncher
+{
+    public static void main(String[] arg)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MyGdxGame(), config);
+    }
 }
--- test/html/src/com/mygdx/game/client/HtmlLauncher.java
+++ test/html/src/com/mygdx/game/client/HtmlLauncher.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
-public class HtmlLauncher extends GwtApplication {
+public class HtmlLauncher extends GwtApplication
+{
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig()
+    {
+        return new GwtApplicationConfiguration(480, 320);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new MyGdxGame();
-        }
+    @Override
+    public ApplicationListener createApplicationListener()
+    {
+        return new MyGdxGame();
+    }
 }
