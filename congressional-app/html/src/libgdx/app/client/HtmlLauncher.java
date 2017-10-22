02c7012883b0b75581a4094acc20253995db320f
KeyboardCreature <duypham12241999@gmail.com>
2017-10-21 22:06:43 -0700

    Reformat Code

--- congressional-app/html/src/libgdx/app/client/HtmlLauncher.java
+++ congressional-app/html/src/libgdx/app/client/HtmlLauncher.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
 import libgdx.app.app;
 
-public class HtmlLauncher extends GwtApplication {
+public class HtmlLauncher extends GwtApplication
+{
 
-        @Override
-        public GwtApplicationConfiguration getConfig () {
-                return new GwtApplicationConfiguration(480, 320);
-        }
+    @Override
+    public GwtApplicationConfiguration getConfig()
+    {
+        return new GwtApplicationConfiguration(480, 320);
+    }
 
-        @Override
-        public ApplicationListener createApplicationListener () {
-                return new app();
-        }
+    @Override
+    public ApplicationListener createApplicationListener()
+    {
+        return new app();
+    }
 }
