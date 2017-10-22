3a994de8c167137835500b96cd552038d8bcc648
KeyboardCreature <duypham12241999@gmail.com>
2017-10-21 21:34:14 -0700

    Created LibGDX project.

---
+++ congressional-app/html/src/libgdx/app/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package libgdx.app.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import libgdx.app.app;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new app();
+        }
+}
