39e7ae0aa82a5da49f8e66505d9de561dbddb19d
KeyboardCreature <duypham12241999@gmail.com>
2017-10-22 01:58:14 -0700

    Revert "Revert "Created Salt Simulator""
    This reverts commit 2ef688c084299c2c089fd01a31f5b7e6164c651b.

---
+++ test/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class DesktopLauncher
+{
+    public static void main(String[] arg)
+    {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Salt Simulator";
+        config.width = 800;
+        config.height = 600;
+        config.resizable = false;
+        new LwjglApplication(new MyGdxGame(), config);
+    }
+}
