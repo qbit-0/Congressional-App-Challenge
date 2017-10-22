e5350cb26e3cfc04dd54fea7dd92f6444c4a7c88
KeyboardCreature <duypham12241999@gmail.com>
2017-10-22 12:12:22 -0700

    Added newline.

--- test/core/src/com/mygdx/game/MyGdxGame.java
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -23,6 +23,7 @@ public class MyGdxGame extends ApplicationAdapter
     {
         Gdx.gl.glClearColor(0, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
