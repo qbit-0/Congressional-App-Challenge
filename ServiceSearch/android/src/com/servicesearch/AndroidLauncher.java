8e041a8c84d10dda9b2db05b544a844a416814b9
KeyboardCreature <duypham12241999@gmail.com>
2017-10-22 14:55:41 -0700

    Created SS

---
+++ ServiceSearch/android/src/com/servicesearch/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.servicesearch;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.servicesearch.ServiceSearch;
+
+public class AndroidLauncher extends AndroidApplication
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        initialize(new ServiceSearch(), config);
+    }
+}
