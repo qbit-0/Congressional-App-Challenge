42b4fc98a950427226532ade56bf55b3915aca52
KeyboardCreature <duypham12241999@gmail.com>
2017-10-22 15:14:37 -0700

    Fixing stuff.

--- ServiceSearch/desktop/src/com/servicesearch/desktop/DesktopLauncher.java
+++ ServiceSearch/desktop/src/com/servicesearch/desktop/DesktopLauncher.java
@@ -4,11 +4,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.servicesearch.ServiceSearch;
 
-public class DesktopLauncher
-{
-    public static void main(String[] arg)
-    {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        new LwjglApplication(new ServiceSearch(), config);
-    }
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ServiceSearch(), config);
+	}
 }
