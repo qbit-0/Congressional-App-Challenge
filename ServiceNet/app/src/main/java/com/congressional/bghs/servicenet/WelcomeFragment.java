cd42d32b127c685a021493e0b0f41f67a368c0ac
KeyboardCreature <duypham12241999@gmail.com>
2017-10-29 20:33:22 -0700

    Welcome update.

--- ServiceNet/app/src/main/java/com/congressional/bghs/servicenet/WelcomeFragment.java
+++ ServiceNet/app/src/main/java/com/congressional/bghs/servicenet/WelcomeFragment.java
@@ -1,11 +1,14 @@
 package com.congressional.bghs.servicenet;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 /**
  * Created by duyph on 10/28/2017.
@@ -13,13 +16,28 @@ import android.view.ViewGroup;
 
 public class WelcomeFragment extends Fragment
 {
-    View myView;
+    Context context;
+    View view;
+    ScrollView mWelcomeScrollView;
+    LinearLayout mWelcomeLayout;
+    LinearLayout mAppointmentSection;
+    LinearLayout mAppointmentContent;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
     {
-        myView = inflater.inflate(R.layout.welcome, container, false);
-        return myView;
+        view = inflater.inflate(R.layout.welcome, container, false);
+        context = view.getContext();
+
+        mWelcomeScrollView = (ScrollView) view.findViewById(R.id.sv_welcome);
+        mWelcomeLayout = (LinearLayout) mWelcomeScrollView.findViewById(R.id.ll_welcome);
+        mAppointmentSection = (LinearLayout) mWelcomeLayout.findViewById(R.id.appointments_section);
+        mAppointmentContent = (LinearLayout) mAppointmentSection.findViewById(R.id.appointment_content);
+
+        if (Variables.matched)
+            mAppointmentContent.setVisibility(View.VISIBLE);
+
+        return view;
     }
 }
