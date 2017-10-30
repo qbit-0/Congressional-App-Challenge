d6c97fd79688695f3b12ecd9ab9158bcd6ebfc12
KeyboardCreature <duypham12241999@gmail.com>
2017-10-29 17:52:26 -0700

    Proximity.

--- ServiceNet/app/src/main/java/com/congressional/bghs/servicenet/ProviderProfileActivity.java
+++ ServiceNet/app/src/main/java/com/congressional/bghs/servicenet/ProviderProfileActivity.java
@@ -2,14 +2,39 @@ package com.congressional.bghs.servicenet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class ProviderProfileActivity extends AppCompatActivity
 {
+    ScrollView mProfileScrollView;
+    LinearLayout mProfileLinearLayout;
+    LinearLayout mTimeSection;
+    LinearLayout mFromSection;
+    TextView mFromDate;
+    TextView mFromTime;
+    LinearLayout mLocSection;
+    LinearLayout mProximitySection;
+    TextView mProximityTextView;
+    Button mConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_profile);
+
+        mProfileScrollView = (ScrollView) findViewById(R.id.sv_profile);
+        mProfileLinearLayout = (LinearLayout) mProfileScrollView.findViewById(R.id.ll_profile);
+        mTimeSection = (LinearLayout) mProfileLinearLayout.findViewById(R.id.time_section);
+        mFromSection = (LinearLayout) mTimeSection.findViewById(R.id.from_section);
+        mFromDate = (TextView) mFromSection.findViewById(R.id.tv_from_date);
+        mFromTime = (TextView) mFromSection.findViewById(R.id.tv_from_time);
+        mLocSection = (LinearLayout) mProfileLinearLayout.findViewById(R.id.loc_section);
+        mProximitySection = (LinearLayout) mLocSection.findViewById(R.id.proximity_section);
+        mProximityTextView = (TextView) mProximitySection.findViewById(R.id.et_proximity);
+        mConfirmButton = (Button) mProfileLinearLayout.findViewById(R.id.btn_confirm);
     }
 }
